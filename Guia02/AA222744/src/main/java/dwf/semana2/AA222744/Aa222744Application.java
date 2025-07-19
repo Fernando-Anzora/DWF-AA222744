715653e226d8514cf72d8546d93523c1e3ee48bb
Fernando-Anzora <fernandoanzora2004@gmail.com>
2025-07-19 16:53:05 -0600

    Guia02

---
+++ Guia02/AA222744/src/main/java/dwf/semana2/AA222744/Aa222744Application.java
@@ -0,0 +1,13 @@
+package dwf.semana2.AA222744;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa222744Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa222744Application.class, args);
+	}
+
+}
---
+++ Guia02/AA222744/src/test/java/dwf/semana2/AA222744/Aa222744ApplicationTests.java
@@ -0,0 +1,13 @@
+package dwf.semana2.AA222744;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aa222744ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
